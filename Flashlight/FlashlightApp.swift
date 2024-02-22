cb2d06f0bc780730ad11e033b70d0acead5f801d
Uhl Albert <bikrrr@gmail.com>
2024-02-22 16:28:07 -0700

    Initial Commit

---
+++ Flashlight/FlashlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashlightApp.swift
+//  Flashlight
+//
+//  Created by Uhl Albert on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
